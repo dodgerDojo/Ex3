af6f6da3163a5719f2eacb678cfa8deb90598ce2
Don Corleone <dan@gmail.com>
2014-05-24 06:02:50 -0700

    first commit

---
+++ ex3.c
@@ -0,0 +1,33 @@
+/********************************/
+
+// Includes:
+
+#include <stdio.h>
+
+/********************************/
+
+// Defines:
+
+#define EXIT_ERROR_CODE      (-1)
+#define EXIT_OK_CODE         (0)
+
+/********************************/
+
+// Static Variables:
+
+/********************************/
+
+// Static Declarations:
+
+/********************************/
+
+// Functions:
+
+/********************************/
+
+// Main:
+
+int main(int argc, char *argv[])
+{
+    return EXIT_OK_CODE;
+}
