6ebc1116265b38191e1338018503cff51e2035d5
Don Corleone <dan@gmail.com>
2014-05-31 08:01:51 -0700

    reads N from queue.

--- ex3.c
+++ ex3.c
@@ -15,6 +15,9 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <errno.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
 
 /********************************/
 
@@ -22,16 +25,42 @@
 
 #define FIFO_NAME           ("ex3FIFO")
 
+#define ALLOW_READ_WRITE_TO_ALL (0666)
+
+#define SHMAT_FAILED        ((void *)-1)
+
+#define NUM_OF_SEMAPHORES   (1)
+#define SEM_INIT            (1)
+
+#define NOF_INPUTS_ERROR    ("wrong number of inputs.\n")
 #define OPEN_ERROR          ("open() failed.\n")
 #define MKFIFO_ERROR        ("mkfifo() failed.\n")
 #define WRITE_ERROR         ("write() failed.\n")
 #define CLOSE_ERROR         ("close() failed.\n")
+#define READ_ERROR          ("read() failed.\n")
 #define SIGACTION_ERROR     ("sigaction() failed.\n")
 #define SIGFILLSET_ERROR    ("sigfillset() failed.\n")
 #define UNLINK_ERROR        ("unlink() failed.\n")
+#define FTOK_ERROR          ("ftok() failed.\n")
+#define SHMGET_ERROR        ("shmget() failed.\n")
+#define SHMAT_ERROR         ("shmat() failed.\n")
+#define SEMGET_ERROR        ("shmget() failed.\n")
+#define SEMCTL_ERROR        ("semctl() failed.\n")
+#define KILL_ERROR          ("kill() failed.\n")
 
 #define EXIT_ERROR_CODE     (-1)
 #define EXIT_OK_CODE        (0)
+
+/********************************/
+
+// Types:
+
+union semun
+{
+    int val;
+    struct semid_ds* buf;
+    unsigned short *array;
+};
 
 /********************************/
 
@@ -50,6 +79,12 @@ static void waitForSignal(void);
 
 static void deleteFifo(void);
 static void writePidToFifo(void);
+
+static key_t createSharedMemory(const char *key_file_name);
+static void createBinarySemaphore(key_t sem_key);
+
+static int readQueueLengthFromFifo(void);
+
 /********************************/
 
 // Functions:
@@ -148,21 +183,127 @@ static void writePidToFifo(void)
     }
 }
 
+static key_t createSharedMemory(const char *key_file_name)
+{
+    const unsigned char KEY_CHAR = 'H';
+    const unsigned int SHM_SIZE_IN_BYTES = 1024;
+
+    int shm_id = 0;
+    char *p_shm_data = NULL;
+
+    // Generate key from the given file
+    key_t shm_key = ftok(key_file_name, KEY_CHAR);
+    if(shm_key < 0)
+    {
+        deleteFifo();
+        // No checking needed, exits with error code.
+        write(STDERR_FILENO, FTOK_ERROR, sizeof(FTOK_ERROR));
+        exit(EXIT_ERROR_CODE);       
+    }
+
+    // Create shared memory
+    shm_id = shmget(shm_key, SHM_SIZE_IN_BYTES, IPC_CREAT|ALLOW_READ_WRITE_TO_ALL);
+    if(shm_id < 0)
+    {
+        deleteFifo();
+        // No checking needed, exits with error code.
+        write(STDERR_FILENO, SHMGET_ERROR, sizeof(SHMGET_ERROR));
+        exit(EXIT_ERROR_CODE);             
+    }
+
+    // Connect to the shared memory.
+    p_shm_data = (char *)shmat(shm_id, 0, 0);
+    if(SHMAT_FAILED == p_shm_data)
+    {
+        deleteFifo();
+        // No checking needed, exits with error code.
+        write(STDERR_FILENO, SHMAT_ERROR, sizeof(SHMAT_ERROR));
+        exit(EXIT_ERROR_CODE);      
+    }
+
+    return shm_key;
+}
+
+static void createBinarySemaphore(key_t sem_key)
+{
+    union semun sem_conf;
+
+    int sem_id = semget(sem_key, NUM_OF_SEMAPHORES, IPC_CREAT|ALLOW_READ_WRITE_TO_ALL);
+    if(sem_id < 0)
+    {
+        deleteFifo();
+        // No checking needed, exits with error code.
+        write(STDERR_FILENO, SEMGET_ERROR, sizeof(SEMGET_ERROR));
+        exit(EXIT_ERROR_CODE);         
+    }
+
+    sem_conf.val = SEM_INIT;
+    if(semctl(sem_id, 0, SETVAL, sem_conf) < 0)
+    {
+        deleteFifo();
+        // No checking needed, exits with error code.
+        write(STDERR_FILENO, SEMCTL_ERROR, sizeof(SEMCTL_ERROR));
+        exit(EXIT_ERROR_CODE);   
+    }
+}
+
+static int readQueueLengthFromFifo(void)
+{
+    int queue_length = 0;
+
+    int fifo_fd = open(FIFO_NAME, O_RDWR);
+    if(fifo_fd < 0)
+    {
+        deleteFifo();
+        // No checking needed, exits with error code.
+        write(STDERR_FILENO, OPEN_ERROR, sizeof(OPEN_ERROR));
+        exit(EXIT_ERROR_CODE);
+    }
+
+    // Read the queue length.
+    if(read(fifo_fd, (void*)&queue_length, sizeof(queue_length)) != sizeof(queue_length))
+    {
+        deleteFifo();
+        // No checking needed, exits with error code.
+        write(STDERR_FILENO, READ_ERROR, sizeof(READ_ERROR));
+        exit(EXIT_ERROR_CODE);
+    }
+
+    // Close FIFO.
+    if(close(fifo_fd) < 0)
+    {
+        deleteFifo();
+        // No checking needed, exits with error code.
+        write(STDERR_FILENO, CLOSE_ERROR, sizeof(CLOSE_ERROR));
+        exit(EXIT_ERROR_CODE);
+    }
+
+    return queue_length;
+}
 /********************************/
 
 // Main:
 
 int main(int argc, char *argv[])
 {
-    const unsigned int ALLOW_READ_WRITE_TO_ALL = 0666;
+    const unsigned int EXPECTED_ARGC = 2;
+    const unsigned int KEY_FILE_ARG_INDEX = 1;
+
+    key_t key = 0;
     int queue_length = 0;
 
     initSigactions();
 
+    if(argc != EXPECTED_ARGC)
+    {
+        // No checking needed, exits with error code.
+        write(STDERR_FILENO, NOF_INPUTS_ERROR, sizeof(NOF_INPUTS_ERROR));
+        exit(EXIT_ERROR_CODE);
+    }
+
     // Create fifo.
     if(mkfifo(FIFO_NAME, ALLOW_READ_WRITE_TO_ALL) < 0)
     {
-        perror("mkfifo()");
         // No checking needed, exits with error code.
         write(STDERR_FILENO, MKFIFO_ERROR, sizeof(MKFIFO_ERROR));
         exit(EXIT_ERROR_CODE);
@@ -175,11 +316,28 @@ int main(int argc, char *argv[])
 
     printf("got signal from %d!\n", Queue_Pid);
 
+    // Create shared memory.
+    key = createSharedMemory(argv[KEY_FILE_ARG_INDEX]);
+
+    // Create semaphore.
+    createBinarySemaphore(key);
+
+    // Notify the queue.out process.
+    if(kill(Queue_Pid, SIGUSR1) < 0)
+    {
+        deleteFifo();
+        // No checking needed, exits with error code.
+        write(STDERR_FILENO, KILL_ERROR, sizeof(KILL_ERROR) - 1);
+        exit(EXIT_ERROR_CODE);
+    }
+
+    queue_length = readQueueLengthFromFifo();
+
+    printf("Got N: %d\n", queue_length);
+
     // Done using the fifo - delete it.
     deleteFifo();
 
-    //createSharedMemory();
-    //createBinarySemaphore();
     //signalSigusr1ToProcess(proc_pid);
     //queue_length = readFromFifo();
 
